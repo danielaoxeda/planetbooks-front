b4b0e0a0afae6014a93533aa1fffc6d684c124ac
HurtadoRonal <ronal_hurtado28@hotmail.com>
2026-04-27 23:47:40 -0500

    Edicion de inicio para llamado de componentes

--- src/app/(with-nav)/page.tsx
+++ src/app/(with-nav)/page.tsx
@@ -1,6 +1,17 @@
+import Hero from "@/components/home/Hero";
+import Certifications from "@/components/home/Certifications";
+import BooksSection from "@/components/home/BooksSection";
+import products from "@/data/products";
 
-export default function Home() {
+export default function Page() {
+    
+    const featured = products.slice(0, 4);
+
     return (
-        <div>Inicio</div>
+        <>
+            <Hero />
+            <Certifications />
+            <BooksSection books={featured} />
+        </>
     );
 }
