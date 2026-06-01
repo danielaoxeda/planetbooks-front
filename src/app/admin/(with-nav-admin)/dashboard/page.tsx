db2e6620261d330174860c06f4d3d1be647c4e22
DanielaFernandaOjedaArrelucea <daniela.oxeda@gmail.com>
2026-06-01 15:57:43 -0500

    feat: connect stats cards to dashboard

---
+++ src/app/admin/(with-nav-admin)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import StatsCards from "@/components/admin/dashboard/StatsCards";
+
+export default function AdminDashboardPage() {
+    return (
+
+        <div className="space-y-8">
+            <StatsCards/>
+        </div>
+
+    );
+}
