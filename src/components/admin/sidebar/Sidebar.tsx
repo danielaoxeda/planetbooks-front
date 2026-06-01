15cd52f7d92535934096e4f993a966a717745236
Kevin Menor <ivanmenorsilva@gmail.com>
2026-05-31 20:50:48 -0500

    add SideBar for Dashboard-user

---
+++ src/components/admin/sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+export default function AdminSidebar() {
+    return (
+        <aside className="w-64 bg-white border-r hidden md:flex flex-col">
+            {/* Logo */}
+
+            {/* Nav */}
+
+            {/* User Profile */}
+        </aside>
+    );
+}
