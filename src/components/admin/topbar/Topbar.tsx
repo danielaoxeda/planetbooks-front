c0cfb9594c6a4efc1db7715a2d45da92f828a45a
Kevin Menor <ivanmenorsilva@gmail.com>
2026-05-31 20:51:50 -0500

    add TopBar for Dashboard-user

---
+++ src/components/admin/topbar/Topbar.tsx
@@ -0,0 +1,11 @@
+export default function AdminTopbar() {
+    return (
+        <header className="h-16 border-b bg-white flex items-center justify-between px-8">
+            {/* Título */}
+
+            {/* Search */}
+
+            {/* Create User */}
+        </header>
+    );
+}
