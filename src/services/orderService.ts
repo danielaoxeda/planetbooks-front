d18db7be0b5d984259c19c7b24a827aff291250a
DanielaFernandaOjedaArrelucea <daniela.oxeda@gmail.com>
2026-06-23 21:39:11 -0500

    refactor: change in ActivityFeed dataAdmin to hooks/useOrders

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,5 +1,26 @@
 import {API_URL} from "@/lib/api";
 
+export async function getOrders() {
+
+    const token = localStorage.getItem("token");
+
+    const res = await fetch(
+        `${API_URL}/orders`,
+        {
+            headers: {
+                ...(token && {
+                    Authorization: `Bearer ${token}`,
+                }),
+            },
+        }
+    );
+
+    if (!res.ok) {
+        throw new Error("Error loading orders");
+    }
+
+    return res.json();
+}
 export async function getOrdersSummary() {
 
     const token =
