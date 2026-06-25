12c5dbcf8f338e0f82e51bb6fb6cf3a8ee07e7de
Kevin Menor <ivanmenorsilva@gmail.com>
2026-06-25 18:00:51 -0500

    feat(auth): add auth types

---
+++ src/types/auth.ts
@@ -0,0 +1,18 @@
+import { User } from "./user";
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface RegisterRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export interface LoginResponse {
+    type: string;
+    token: string;
+    user: User;
+}
