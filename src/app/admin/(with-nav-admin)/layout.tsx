3151703d9d70990230aa2d4ad5fc96560bff0e46
Kevin Menor <ivanmenorsilva@gmail.com>
2026-06-26 21:13:50 -0500

    Add AuthGuard for route protection

--- src/app/admin/(with-nav-admin)/layout.tsx
+++ src/app/admin/(with-nav-admin)/layout.tsx
@@ -73,6 +73,7 @@ export default function RootLayout({
         >
         <body className="min-h-full flex flex-col md:flex-row overflow-x-hidden">
         <AuthProvider>
+
             <CartProvider>
                 <Sidebar
                     open={sidebarOpen}
