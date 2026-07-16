4c587ce4c49eac00f6a83bb5add9f3669c51dbdd
DanielaFernandaOjedaArrelucea <daniela.oxeda@gmail.com>
2026-07-15 20:36:05 -0500

    add cloudfare to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* user options here */
-  reactCompiler: true,
+    reactCompiler: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "pub-27fff5d908ef401e865968062d72cbb2.r2.dev",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
